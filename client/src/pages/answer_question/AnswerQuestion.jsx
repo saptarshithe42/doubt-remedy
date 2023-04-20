7be5a824449755ab0415e70baacc4582c7e0a58f
Saptarshi Mondal <saptarshi.the.42@gmail.com>
2023-04-20 17:04:31 +0530

    answer question page

--- client/src/pages/answer_question/AnswerQuestion.jsx
+++ client/src/pages/answer_question/AnswerQuestion.jsx
@@ -8,7 +8,27 @@ import { useParams } from "react-router-dom"
 import QuestionView from "../../components/QuestionView"
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
+
+const modules = {
+    toolbar: [
+        [{ 'header': [1, 2, 3, 4, 5, 6] }],
+        [{ size: [] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+        [{ align: '' }, { align: 'center' }, { align: 'right' }, { align: 'justify' }],
+        ['image']
+    ],
+}
+
+const formats = [
+    'header', 'font', 'size',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image', 'align'
+]
 
 
 
@@ -17,6 +37,7 @@ function AnswerQuestion() {
     const { id } = useParams()
 
     const [question, setQuestion] = useState(null)
+    const [canAnswer, setCanAnswer] = useState(true)
 
     const fetchQuestion = async (id) => {
 
@@ -31,6 +52,10 @@ function AnswerQuestion() {
             const data = await res.json();
 
             console.log(data);
+
+            if (data.answers.length == 2) {
+                setCanAnswer(false);
+            }
 
             setQuestion(data)
 
@@ -56,10 +81,27 @@ function AnswerQuestion() {
     return (
         <div>
             <ToastContainer />
-            <h2 style={{margin : "1rem"}}>Question :-</h2>
+            <h2 style={{ margin: "1rem" }}>Question :-</h2>
             {question && <QuestionView question={question} />}
 
-            
+            {canAnswer &&
+                <div className="answer-div">
+                    <div className="container editor-div w-75">
+                        <ReactQuill
+                            theme="snow"
+                            value={question}
+                            onChange={setQuestion}
+                            modules={modules}
+                            formats={formats}
+
+                        />
+
+                        <button className="btn btn-primary">Submit</button>
+                    </div>
+
+                </div>
+            }
+
         </div>
     )
 }
